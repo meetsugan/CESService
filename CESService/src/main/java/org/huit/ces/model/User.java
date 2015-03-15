6add6cd25b5c94debf04dde22e616092c77a1973
Saravanan Krishnaraj <saravanan.krishnaraj@gmail.com>
2015-03-15 03:17:29 -0400

    Adding in skeleton service

---
+++ CESService/src/main/java/org/huit/ces/model/User.java
@@ -0,0 +1,37 @@
+package org.huit.ces.model;
+
+public class User {
+
+	private String harvardID;
+	private String title;
+	private String courseRole;
+	private String courseID;
+	
+	public String getHarvardID() {
+		return harvardID;
+	}
+	public void setHarvardID(String harvardID) {
+		this.harvardID = harvardID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCourseRole() {
+		return courseRole;
+	}
+	public void setCourseRole(String courseRole) {
+		this.courseRole = courseRole;
+	}
+	public String getCourseID() {
+		return courseID;
+	}
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+	
+	
+	
+}
--- CESService/src/main/webapp/index.jsp
+++ CESService/src/main/webapp/index.jsp
@@ -1,8 +1,6 @@
 <html>
 <body>
-    <h2>Jersey RESTful Web Application!</h2>
-    <p><a href="webapi/myresource">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
+    <h2>HUIT Course Evaluation Service</h2>
+    <p><a href="api/">CES Web API</a></p>
 </body>
 </html>
